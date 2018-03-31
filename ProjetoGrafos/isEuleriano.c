be80f4dc4c4dca1714acaf35f6f1ded9f473e644
Pedro Henrique M. Da Silva <pedrohmsilva29@hotmail.com>
2018-03-31 12:13:20 -0300

    Add files via upload

--- ProjetoGrafos/isEuleriano.c
+++ ProjetoGrafos/isEuleriano.c
@@ -3,23 +3,24 @@
 #include <memory.h>
 
 #define MAX_FILA  100
+#define false 3
+#define true 4
 typedef int bool;
-
-#define true 1
-#define false 0
 
 #define NONE  -1
 #define BRANCO 0
 #define CINZA  1
 #define PRETO  2
 
-typedef struct aresta{
+typedef struct aresta {
 	int nome;
 	struct aresta *prox;
+	int verts[2];
 }Aresta;
 
-typedef struct vert{
+typedef struct vert {
 	int nome;
+	int cor;
 	Aresta *prim;
 }Vertice;
 
@@ -32,62 +33,110 @@ int  calcumaTamanho(Vertice G[], int ordem);
 /* IsEuleriano = IsConexo + Para todo g(VG), V é par */
 
 
-void criaGrafo(Vertice **G, int ordem){
+void criaGrafo(Vertice **G, int ordem) {
 	int i;
-	*G= (Vertice*) malloc(sizeof(Vertice)*ordem);
-	
-	for(i=0; i<ordem; i++){
-		(*G)[i].nome= i;
-		(*G)[i].prim= NULL;
+	*G = (Vertice*)malloc(sizeof(Vertice)*ordem);
+
+	for (i = 0; i<ordem; i++) {
+		(*G)[i].nome = i;
+		(*G)[i].prim = NULL;
 	}
 }
 
-int acrescentaAresta(Vertice G[], int ordem, int v1, int v2){
+int acrescentaAresta(Vertice G[], int ordem, int v1, int v2) {
 	if (v1<0 || v1 >= ordem)
-	   return 0;
+		return 0;
 	if (v2<0 || v2 >= ordem)
-	   return 0;
-	
-	Aresta *A1= (Aresta *) malloc(sizeof(Aresta));
-	A1->nome= v2;
-	A1->prox= G[v1].prim;
-	G[v1].prim= A1;
+		return 0;
 
-	Aresta *A2= (Aresta *) malloc(sizeof(Aresta));
-	A2->nome= v1;
-	A2->prox= G[v2].prim;
-	G[v2].prim= A2;
+	Aresta *A1 = (Aresta *)malloc(sizeof(Aresta));
+	A1->nome = v2;
+	A1->prox = G[v1].prim;
+	A1->verts[0] = v1;
+	A1->verts[1] = v2;
+	G[v1].prim = A1;
+
+	Aresta *A2 = (Aresta *)malloc(sizeof(Aresta));
+	A2->nome = v1;
+	A2->prox = G[v2].prim;
+	A2->verts[0] = v1;
+	A2->verts[1] = v2;
+	G[v2].prim = A2;
+
+	
 }
 
 
-int  calcumaTamanho(Vertice G[], int ordem){
+int  calcumaTamanho(Vertice G[], int ordem) {
 	int i;
-	int totalArestas=0;
-	
-	for (i=0; i<ordem; i++){
+	int totalArestas = 0;
+
+	for (i = 0; i<ordem; i++) {
 		int j;
-		Aresta *aux= G[i].prim;
-		for(j=0; aux != NULL; aux= aux->prox, j++);
-		totalArestas += j;	
+		Aresta *aux = G[i].prim;
+		for (j = 0; aux != NULL; aux = aux->prox, j++);
+		totalArestas += j;
 	}
-	return totalArestas/2 + ordem;
+	return totalArestas / 2 + ordem;
 }
 
-void imprimeGrafo(Vertice G[], int ordem){
+void imprimeGrafo(Vertice G[], int ordem) {
 	int i;
 
-	for (i=0; i<ordem; i++){
+	for (i = 0; i<ordem; i++) {
 		printf("\nV%d: ", i);
-		Aresta *aux= G[i].prim;
-		for( ; aux != NULL; aux= aux->prox)
+		Aresta *aux = G[i].prim;
+		for (; aux != NULL; aux = aux->prox)
 			printf("%3d", aux->nome);
 	}
 	printf("\n\n");
 }
 
-bool isConexo(Vertice G[], int ordem){
-		//noImplementationError
-		return true;
+bool isConexo(Vertice G[], int ordem) {
+	int i,v1,v2,j,k;
+	for (i = 0; i < ordem; i++) {
+		G[i].cor = BRANCO;
+	}
+
+	for (i = 0; i<ordem;) {
+		Aresta *aux = G[i].prim;
+		if(i == 0)
+			G[0].cor = CINZA;
+		for (; aux != NULL; aux = aux->prox){
+			if (G[i].cor != CINZA)
+				return false;
+
+			v1 = aux->verts[0];
+			v2 = aux->verts[1];
+			if(v1 != i && G[v1].cor != PRETO)
+				G[v1].cor = CINZA;
+			if (v2 != i && G[v2].cor != PRETO)
+				G[v2].cor = CINZA;
+
+		}
+		G[i].cor = PRETO;
+
+		for (j = 0; j < ordem; j++) {
+			if (G[j].cor == CINZA) {
+				i = j;
+				break;
+			}
+		}
+		k = 0;
+		for (j = 0; j < ordem; j++) 
+			if (G[j].cor == PRETO) 
+				k++;
+
+		if (k == ordem)
+			return true;
+		
+	}
+
+	for (i = 0; i < ordem; i++)
+		if (G[i].cor != PRETO)
+			return false;
+
+	return false;
 }
 
 bool isVerticesPares(Vertice G[], int ordem) {
@@ -109,39 +158,39 @@ bool isVerticesPares(Vertice G[], int ordem) {
 	return true;
 }
 
-bool isEuleriano(Vertice G[], int ordemG){
+bool isEuleriano(Vertice G[], int ordemG) {
 	bool conexo = isConexo(G, ordemG);
 	bool pares = isVerticesPares(G, ordemG);
-	
-	if(conexo && pares)
+
+	if (conexo && pares)
 		return true;
 	else
 		return false;
-	
+
 }
 
 
 int main(int argc, char *argv[]) {
 	Vertice *G;
-	int ordemG= 10;
+	int ordemG = 5;
 
-		
+
 	criaGrafo(&G, ordemG);
-	acrescentaAresta(G,ordemG,3,4);
-	acrescentaAresta(G,ordemG,4,2);
-	acrescentaAresta(G,ordemG,5,4);
-	acrescentaAresta(G,ordemG,2,3);
-	acrescentaAresta(G,ordemG,3,7);
-	
-	printf("\nTamanho: %d\n",calcumaTamanho(G, ordemG));
+	acrescentaAresta(G, ordemG, 0, 4);
+	acrescentaAresta(G, ordemG, 0, 1);
+	acrescentaAresta(G, ordemG, 2, 3);
+	acrescentaAresta(G, ordemG, 4, 2);
+	acrescentaAresta(G, ordemG, 1, 3);
+
+	printf("\nTamanho: %d\n", calcumaTamanho(G, ordemG));
 
 	imprimeGrafo(G, ordemG);
-	
-	if(isEuleriano(G, ordemG) == true)
+
+	if (isEuleriano(G, ordemG) == true)
 		printf("Grafo G e Euleriano!");
 	else
 		printf("Grafo G nao e Euleriano!");
-	
+
 
 	return 0;
 }
